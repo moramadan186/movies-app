03ddef034fb1cae1f0025b106632fc1d473d9f93
Mohamed Ramadan <mr01028760097@gmail.com>
2022-06-27 22:37:09 +0200

    api config

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '',
+    apiKey: '4cfb881a943ba5c6b1eec70ac8f3298d',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
