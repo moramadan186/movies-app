8772922a3b8a341cb40b3cafa6c162682dfd6235
Mohamed Ramadan <mr01028760097@gmail.com>
2022-06-27 22:36:44 +0200

    add app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.scss';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
 
+import { BrowserRouter } from "react-router-dom";
+
+import AppRoutes from "./config/AppRoutes";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
-      my app
+      <Header />
+      <AppRoutes />
+      <Footer />
     </div>
   );
 }
