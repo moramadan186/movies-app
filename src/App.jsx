11bb92a6802721b4612ca3bd97816c5a1b9bc94f
Mohamed Ramadan <mr01028760097@gmail.com>
2022-06-30 04:10:05 +0200

    make page footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import AppRoutes from "./config/AppRoutes";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 import "swiper/swiper.min.css";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./App.scss";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import "./footer.scss";
+import { Link } from "react-router-dom";
+import background from "../../assets/footer-bg.jpg";
+import logo from "../../assets/tmovie.png";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer" style={{ backgroundImage: `url(${background})` }}>
+      <div className="container footer__content ">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <Link to="/">tMovies</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Terms of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Pravacy policy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
